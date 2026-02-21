4f85fe51da9777153e230f9c204c320b62b60600
KevinFigenshu <kfig21@gmail.com>
2026-02-20 19:45:01 -0500

    Configure for Heroku

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './UI/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/masters-pool">
+    <BrowserRouter>
       <ScoreProvider>
         <App />
       </ScoreProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/masters-pool/', // NECESSARY FOR GITHUB PAGES
+  // base: '/masters-pool/', // NECESSARY FOR GITHUB PAGES (doesnt work with browser router tho :( )
+  base: '/',
 });
