894b71c4f6da9f7dab80aec92d53e9c2e643a46f
KevinFigenshu <kfig21@gmail.com>
2026-04-07 15:21:45 -0400

    remove un used tournaments

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,19 @@
 import { eventsEnum } from './enums.ts';
 import { mastersEventData } from './events/masters/eventData.ts';
 import type { EventMatrix } from '../types/event.ts';
-import { genesisEventData } from './events/genesis/eventData.ts';
-import { cognizantEventData } from './events/cognizant/eventData.ts';
-import { arnoldPalmerEventData } from './events/arnold_palmer/eventData.ts';
-import { houstonEventData } from './events/houston/eventData.ts';
-import { texasEventData } from './events/texas/eventData.ts';
+// import { genesisEventData } from './events/genesis/eventData.ts';
+// import { cognizantEventData } from './events/cognizant/eventData.ts';
+// import { arnoldPalmerEventData } from './events/arnold_palmer/eventData.ts';
+// import { houstonEventData } from './events/houston/eventData.ts';
+// import { texasEventData } from './events/texas/eventData.ts';
 
 export const EVENT_MATRIX: EventMatrix = {
   [eventsEnum.MASTERS]: mastersEventData,
-  [eventsEnum.GENESIS]: genesisEventData,
-  [eventsEnum.COGNIZANT]: cognizantEventData,
-  [eventsEnum.ARNOLD_PALMER]: arnoldPalmerEventData,
-  [eventsEnum.HOUSTON]: houstonEventData,
-  [eventsEnum.TEXAS]: texasEventData,
+  // [eventsEnum.GENESIS]: genesisEventData,
+  // [eventsEnum.COGNIZANT]: cognizantEventData,
+  // [eventsEnum.ARNOLD_PALMER]: arnoldPalmerEventData,
+  // [eventsEnum.HOUSTON]: houstonEventData,
+  // [eventsEnum.TEXAS]: texasEventData,
 };
 
 // IMPORTANT: KEEP THESE UPDATED
